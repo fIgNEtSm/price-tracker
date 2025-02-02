37f59f3344e0a9f1812ecb870ca302312ccb1420
Vishwateja Domakonda <vishwatejadomakonda@gmail.com>
2025-02-02 12:33:03 +0530

    Update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, flash
+from database import init_db, add_tracking, tracking_exists, get_all
 
 app = Flask(__name__)
 app.secret_key = "vishwaisgreat"
+
+init_db(app=app)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -11,9 +14,12 @@ def home():
         email = request.form['email']
         if not url or not price or not email:
             flash("All fields are required", "danger")
+        elif tracking_exists(url=url, email=email):
+            flash("You are already tracking this product.", "success")
         else:
-            flash("Tracking started successfully", "success")
-            print(url, price, email)
+            add_tracking(url=url, price=price, email=email)
+            flash("Tracking started successfully.", "success")
+        print(get_all())
     return render_template("main_page.html")
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,9 +1,30 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-import os
 
-class Base(DeclarativeBase):
-    pass
 
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
+
+def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://price_tracker_postgresql_user:yA04ULdjNEwDWd2fPpuVAvDKvMkBL9XN@dpg-cueg54lds78s73ae06s0-a.singapore-postgres.render.com/price_tracker_postgresql'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+class PriceTracker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
+def add_tracking(url, price, email):
+    new_entry = PriceTracker(url=url, price=price, email=email)
+    db.session.add(new_entry)
+    db.session.commit()
+
+def tracking_exists(url, email):
+    return PriceTracker.query.filter_by(url=url, email=email).first() is not None
+
+def get_all():
+    trackers = PriceTracker.query.all()
+    return [{"id": tracker.id, "email": tracker.email, "price": tracker.price, "url": tracker.url} for tracker in trackers]
